87cc9a45751fffc928f31c32e343252df8f35c4f
Admin <you@example.com>
2019-11-29 15:04:09 +0530

    ADD:Open csv Read and Parse to Bean

--- src/main/java/com/opencsv/OpenCSVReadParseToBean.java
+++ src/main/java/com/opencsv/OpenCSVReadParseToBean.java
@@ -10,5 +10,27 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class OpenCSVReadParseToBean {
+    private static final String SAMPLE_CSV_FILE_PATH = "/home/admin1/IdeaProjects/OpenCSVProject/src/main/resources/users-with-header.csv";
 
+    public static void main(String[] args) throws IOException {
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+        ) {
+            CsvToBean<CSVUser> csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(CSVUser.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            Iterator<CSVUser> csvUserIterator = csvToBean.iterator();
+
+            while (csvUserIterator.hasNext()) {
+                CSVUser csvUser = csvUserIterator.next();
+                System.out.println("Name : " + csvUser.getName());
+                System.out.println("Email : " + csvUser.getEmail());
+                System.out.println("PhoneNo : " + csvUser.getPhoneNo());
+                System.out.println("Country : " + csvUser.getCountry());
+                System.out.println("==========================");
+            }
+        }
+    }
 }
